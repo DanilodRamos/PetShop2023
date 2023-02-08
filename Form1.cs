33b76995678f7e8d93baaab3835e3c872c49e8b3
danilo <danilodramos@yahoo.com.br>
2023-02-08 18:03:42 -0300

    iniciado criado botoes e fotos

--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,55 @@ namespace PetShop2023
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnServicos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja mesmo sair?", "PetShop", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                Application.Exit();
+        }
+
+        private void btnmin_Click(object sender, EventArgs e)
+        {
+            //minimizar
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCliente_Click(object sender, EventArgs e)
+        {
+            frmCadCliente cliente = new frmCadCliente();
+            cliente.TopLevel = false;
+            cliente.Dock = DockStyle.Fill;
+            panelCentral.Controls.Clear(); //limpa o painel
+            panelCentral.Controls.Add(cliente);//adiciona o form no painel
+            cliente.Show();//apareca
+
+            //aplica a posicao do eixo X do botao ao panelSelecao
+
+            panelSelecao.Top = btnCliente.Top;
+        }
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            panelCentral.Controls.Clear();
+            panelSelecao.Top = btnInicio.Top;
+        }
     }
 }
+
+
